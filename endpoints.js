68212f61e72636b5976f8e7b7bdc55156cad63d1
Alison Ramos da Silva <alison.silva@dbserver.com.br>
2021-06-12 16:43:38 -0300

    Criado endpoint post users + documentacao swagger

--- endpoints.js
+++ endpoints.js
@@ -25,4 +25,26 @@ module.exports = function (app) {
         }
         return res.status(200).send(data)
     })
+
+    app.post('/users', (req, res) => {
+        /* #swagger.tags = ['User']
+           #swagger.description = 'Endpoint para adicionar um usuário.' */
+
+        /* #swagger.parameters['newUser'] = {
+               in: 'body',
+               description: 'Informações do usuário.',
+               required: true,
+               type: 'object',
+               schema: { $ref: "#/definitions/AddUser" }
+        } */
+
+        const newUser = req.body
+
+        if (true) {
+            // #swagger.responses[201] = { description: 'Usuário registrado com sucesso!' }
+            return res.status(201).send(data)
+        }
+        return res.status(500)    // #swagger.responses[500]
+    })
+
 }
